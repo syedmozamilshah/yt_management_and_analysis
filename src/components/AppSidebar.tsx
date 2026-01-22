f6cb61fb555f445e9f6242ecf5af729997a4e3d5
Syed Mozamil Shah <syedmozamilshah99@gmail.com>
2026-01-22 23:40:59 +0500

    Fix: Bigger aligned sidebar logo and off-white AI button boxes

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -206,9 +206,9 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          {/* Logo - left aligned and properly sized */}
-          <div className="px-2 py-3 mb-2">
-            <img src="/Blow_me_ai.png" alt="Blow Me AI" className="h-12 w-auto" />
+          {/* Logo - left aligned with Add Channel button */}
+          <div className="px-2 pt-4 pb-3 mb-1">
+            <img src="/Blow_me_ai.png" alt="Blow Me AI" className="h-14 w-auto" />
           </div>
           {/* Add Channel Button - Top of sidebar */}
           <div className="px-2 mb-3">
